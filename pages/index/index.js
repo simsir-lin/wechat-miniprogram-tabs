967a805b215181ec6c4cd88b5efb228763a24d38
simsir-lin <15986907592@163.com>
2018-02-07 15:35:33 +0800

    上传本地代码

---
+++ pages/index/index.js
@@ -0,0 +1,12 @@
+Page(Object.assign({}, {}, {
+  data: {
+  },
+  handleTabClick: function (e) {
+    console.log(e.detail.index)
+  },
+  onLoad: function (options) {
+  },
+  onShareAppMessage: function () {
+
+  }
+}))
